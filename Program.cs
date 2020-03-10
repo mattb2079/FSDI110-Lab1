9da5d8c3dd3efdbbd0416dda8d8fb5838d35b65e
mattb2079 <mattb2079@yahoo.com>
2020-03-09 19:27:59 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test();
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("This is a method");
+
+            /* Variables and Data types */
+            string firstName = "Matt";
+            Console.WriteLine("My name is " + firstName);
+
+            int age = 99;
+            long seconds = 1298031829031831;
+
+            float price = 12.34f;
+
+            decimal price2 = 123.23m;
+
+            bool isThiscrazy = true;
+
+            // if statements
+            if(isThiscrazy)
+            {
+                Console.WriteLine("Yeah it is");
+            }
+            else{
+                Console.WriteLine("No, it's not");
+            }
+
+            // loops
+            for(int i=0; i<10; i++){
+                Console.WriteLine("Iteration: " + i);
+            }
+
+        }
+    }
+}
